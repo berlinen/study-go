45595b44b8431cacc579ad4e86c0d31ee4a22f8d
berlinen <1292393108@qq.com>
2019-08-26 17:47:08 +0800

    err

--- baseVar.go
+++ baseVar.go
@@ -74,6 +74,17 @@ fmt.Printf("%s\n", s)
 m := `hello
 	world`
 
+// error 类型
+
+// Go内置有一个error类型，专门用来处理错误信息，Go的package里面还专门有一个包errors来处理错误：
+
+err := errors.New("emit macho dwarf: elf header corrupted")
+if err != nil {
+	fmt.print(err)
+}
+
+
+
 
 
 
