aea1fd42fe5bd7d4812062c0b3eacb0b92004137
berlinen <1292393108@qq.com>
2019-08-27 18:33:59 +0800

    map

--- array.go
+++ array.go
@@ -126,3 +126,38 @@ slice := array[2:4]  // 这个例子里面slice的容量是8，新版本里面�
 
 slice = array[2:4:7] // 上面这个的容量就是7-2，即5。这样这个产生的新的slice就没办法访问最后的三个元素。
 // 如果slice是这样的形式array[:i:j]，即第一个参数为空，默认值就是0。
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+// map
+
+// map也就是Python中字典的概念，它的格式为map[keyType]valueType
+
+// 我们看下面的代码，map的读取和设置也类似slice一样，通过key来操作，只是slice的index只能是｀int｀类型，而map多了很多类型，可以是int，可以是string及所有完全定义了==与!=操作的类型。
+
+// 声明一个key是字符串，值为int的字典,这种方式的声明需要在使用之前使用make初始化
+var numbers map[string]int
+
+// 另一种map的声明方式
+numbers = make(map[string]int)
+numbers["one"] = 1  //赋值
+numbers["ten"] = 10 //赋值
+numbers["three"] = 3
+
+fmt.Println("第三个数字是: ", numbers["three"]) // 读取数据
+// 打印出来如:第三个数字是: 3
+
+// 这个map就像我们平常看到的表格一样，左边列是key，右边列是值
+
+//使用map过程中需要注意的几点：
+
+/*
+	map是无序的，每次打印出来的map都会不一样，它不能通过index获取，而必须通过key获取
+	map的长度是不固定的，也就是和slice一样，也是一种引用类型
+	内置的len函数同样适用于map，返回map拥有的key的数量
+	map的值可以很方便的修改，通过numbers["one"]=11可以很容易的把key为one的字典值改为11
+	map和其他基本型别不同，它不是thread-safe，在多个go-routine存取时，必须使用mutex lock机制
+	map的初始化可以通过key:val的方式初始化值，同时map内置有判断是否存在key的方式
+*/
+
+通过delete删除map的元素：
