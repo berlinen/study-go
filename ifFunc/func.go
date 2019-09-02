9924592b929b182b8b580b2ea36f9c64fc5f7c2c
berlinen <1292393108@qq.com>
2019-09-02 15:55:20 +0800

    update

--- ifFunc/func.go
+++ ifFunc/func.go
@@ -7,3 +7,13 @@ func funcName(input1 type1, input2 type2) (output1 type1, output2 type2) {
 	//返回多个值
 	return value1, value2
 }
+
+上面的代码我们看出
+
+关键字func用来声明一个函数funcName
+函数可以有一个或者多个参数，每个参数后面带有类型，通过,分隔
+函数可以返回多个值
+上面返回值声明了两个变量output1和output2，如果你不想声明也可以，直接就两个类型
+如果只有一个返回值且不声明返回值变量，那么你可以省略 包括返回值 的括号
+如果没有返回值，那么就直接省略最后的返回信息
+如果有返回值， 那么必须在函数的外层添加return语句
