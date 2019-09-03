1a97691f6c5b98bdaab95e9bd14e9c06a4927fa1
berlinen <1292393108@qq.com>
2019-09-03 14:36:15 +0800

    update

--- ifFunc/func.go
+++ ifFunc/func.go
@@ -159,3 +159,24 @@ Go语言中channel，slice，map这三种类型的实现机制类似指针，所
 
 
 defer
+
+Go语言中有种不错的设计，即延迟（defer）语句，你可以在函数中添加多个defer语句。当函数执行到最后时，这些defer语句会按照逆序执行，最后该函数返回。特别是当你在进行一些打开资源的操作时，遇到错误需要提前返回，在返回前你需要关闭相应的资源，不然很容易造成资源泄露等问题。如下代码所示，我们一般写打开一个资源是这样操作的：
+
+func ReadWrite() bool {
+	file.Open("file")
+// 做一些工作
+	if failureX {
+		file.Close()
+		return false
+	}
+
+	if failureY {
+		file.Close()
+		return false
+	}
+
+	file.Close()
+	return true
+}
+
+
