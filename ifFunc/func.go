d9c1f87d4082cd174b3f9754facec685d0de061d
berlinen <1292393108@qq.com>
2019-09-03 14:39:33 +0800

    paincExample

--- ifFunc/func.go
+++ ifFunc/func.go
@@ -272,3 +272,15 @@ func init() {
 		panic("no value for $USER")
 	}
 }
+
+下面这个函数检查作为其参数的函数在执行时是否会产生panic：
+
+func throwsPanic(f func()) (b bool) {
+	defer func() {
+		if x := recover(); x != nil {
+			b = true
+		}
+	}()
+	f() //执行函数f，如果f中出现了panic，那么就可以恢复回来
+	return
+}
